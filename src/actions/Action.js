9380c6a5b16187ecf4dbf99eaa4e9c65f9b56a69
Eldo Magan <magan.eldo@gmail.com>
2018-11-05 12:31:09 +0100

    Pass record to generateId function

--- src/actions/Action.js
+++ src/actions/Action.js
@@ -48,7 +48,7 @@ export default class Action {
 
       keys.forEach((key) => {
         if (!record[key]) {
-          record[key] = context.options.generateId();
+          record[key] = context.options.generateId(record);
         }
       });
 
